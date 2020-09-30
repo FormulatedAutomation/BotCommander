6cfe85aa3af09d0252be498ae4f6226dbb4caf44
Mark Percival <m@mdp.im>
2020-09-30 01:21:22 +0000

    Fix api signatures

--- pages/api/botcommand/jobs/[botId]/[runId]/artifacts.ts
+++ pages/api/botcommand/jobs/[botId]/[runId]/artifacts.ts
@@ -3,12 +3,14 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { BotCommandContext, ensureLoggedIn } from '../../../../../../server/middleware/context'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse, context: BotCommandContext) => {
-  const {sources, bots} = context
+  const { bots } = context
   const id: string = Array.isArray(req.query.runId) ? req.query.runId[0]: req.query.runId
   const botId: string = Array.isArray(req.query.botId) ? req.query.botId[0]: req.query.botId
   const runId: string = Array.isArray(req.query.runId) ? req.query.runId[0]: req.query.runId
+  const bot = bots.find(({id}) => botId === id )
 
-  const job = new RoboCloudJob(botId, bots[botId], id)
+  const job = new RoboCloudJob(id, bot)
+
   const response = await job.artifacts(runId)
   res.statusCode = 200
   res.json(response)
--- pages/api/botcommand/jobs/[botId]/[runId]/artifacts/[artifactId]/[filename].tsx
+++ pages/api/botcommand/jobs/[botId]/[runId]/artifacts/[artifactId]/[filename].tsx
@@ -3,14 +3,15 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { BotCommandContext, ensureLoggedIn } from '../../../../../../../../server/middleware/context'
 
 const handler = async (req: NextApiRequest, res: NextApiResponse, context: BotCommandContext) => {
-  const {sources, bots} = context
+  const {bots} = context
   const id: string = Array.isArray(req.query.runId) ? req.query.runId[0]: req.query.runId
   const botId: string = Array.isArray(req.query.botId) ? req.query.botId[0]: req.query.botId
   const runId: string = Array.isArray(req.query.runId) ? req.query.runId[0]: req.query.runId
   const artifactId: string = Array.isArray(req.query.artifactId) ? req.query.artifactId[0]: req.query.artifactId
   const filename: string = Array.isArray(req.query.filename) ? req.query.filename[0]: req.query.filename
+  const bot = bots.find(({id}) => botId === id )
 
-  const job = new RoboCloudJob(botId, bots[botId], id)
+  const job = new RoboCloudJob(id, bot)
   const response = await job.getArtifact(runId, artifactId, filename)
   if (response.status === 200) {
     res.setHeader('content-type', response.headers.get('content-type'))
