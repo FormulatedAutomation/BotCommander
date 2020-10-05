ca242e5bf01f9a45058ce0e6793ade57c458a397
Mark Percival <m@mdp.im>
2020-10-05 19:07:52 +0000

    Args passed correctly again
    - Fixed uncontrolled render error

--- components/InputArgsFields.tsx
+++ components/InputArgsFields.tsx
@@ -24,11 +24,13 @@ const InputArgsFields: FunctionComponent<{ inputArgs: InputArgs[], inputArgsChan
     <div>
       {Object.values(inputArgs).map(inputArg => (
         <div key={`input-${inputArg.name}`}>
-          <label htmlFor="{inputArg.name}" className="block text-sm font-medium leading-5 text-gray-700">{inputArg.name}</label>
+          <label htmlFor={inputArg.name} className="block text-sm font-medium leading-5 text-gray-700">{inputArg.name}</label>
           <div className="mt-1 relative rounded-md shadow-sm">
-            <input id="{inputArg.name}"
-              value={inputValues[inputArg.name]}
+            <input id={inputArg.name}
+              value={inputValues[inputArg.name] || ''}
               onChange={handleChange}
+              name={inputArg.name}
+              type="text"
               className="form-input block w-full sm:text-sm sm:leading-5" />
           </div>
         </div>
